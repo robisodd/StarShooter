b0a08228b10894d9a0c50e2436764118d6544315
robisodd <robisodd@gmail.com>
2014-12-28 15:48:57 -0500

    Kinda Rotates

--- src/main.c
+++ src/main.c
@@ -9,20 +9,29 @@ static bool sl_button_depressed = false;
 static Window *main_window;
 static Layer *graphics_layer;
 
-#define MAX_STARS 200
-struct starstruct {
+struct xyz {
 	uint8_t x;
   uint8_t y;
   uint8_t z;
-} *star[MAX_STARS];
+};
+
+#define MAX_STARS 200
+struct xyz *star[MAX_STARS];
+
+struct {
+	int8_t x;
+  int8_t y;
+  int8_t z;
+} speed;
 
 struct playerstruct {
-  uint8_t x;
-  uint8_t y;
-  uint8_t speed;
+  struct xyz pos;
+  struct xyz spd;
+  struct xyz fac;
 } player;
 
 int16_t sign16(int16_t x) {return ((x > 0) - (x < 0));}
+uint16_t a=0, b=0;
 
 // ------------------------------------------------------------------------ //
 //  Timer Functions
@@ -31,30 +40,45 @@ static void timer_callback(void *data) {
   AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
   accel_service_peek(&accel);        // Read accelerometer
 
-  int16_t targetspeed;
-  if(up_button_depressed) {        //     up = megaboost
-    targetspeed = 32;
-  } else if(sl_button_depressed) { // select = boost
-    targetspeed = 16;
-  } else if(dn_button_depressed) { //   down = brakes
-    targetspeed = 0;
-  } else {
-    targetspeed = 1;
-  }
-  player.speed += sign16(targetspeed - player.speed);
-  
+ 
   for(uint16_t i = 0; i < MAX_STARS; i++) {
-    if(star[i]->z <= player.speed) {
-      star[i]->x = rand() & 255;
-		  star[i]->y = rand() & 255;
-      star[i]->z = (rand() % 255)+1;
-    } else {
-     star[i]->z -= player.speed;
-    }
+    star[i]->x -= speed.x;
+    star[i]->y -= speed.y;
+    star[i]->z -= speed.z;
   }
-
-  player.x += accel.x>>6;
-  player.y -= accel.y>>6;
+  
+  if(up_button_depressed) { // stop rotation, but don't stop movement
+    a=0; b=0;
+  }
+  
+  if(dn_button_depressed) { // move
+    a += accel.x;
+    b -= accel.y;
+  }
+  
+  if(sl_button_depressed) { // thrust
+    int32_t px, py, pz;
+    int32_t qx, qy, qz;
+    
+    px = 0;
+    py = 0;
+    pz = 1024;
+    
+    qx = ((px * cos_lookup(a)) + (pz * sin_lookup(a))) / TRIG_MAX_RATIO;
+    qy = py;
+    qz = ((px * -sin_lookup(a)) + (pz * cos_lookup(a))) / TRIG_MAX_RATIO;
+    
+    px = qx;
+    py = ((qy * cos_lookup(b)) + (qz * -sin_lookup(b))) / TRIG_MAX_RATIO;
+    pz = ((qy * sin_lookup(b)) + (qz * cos_lookup(b))) / TRIG_MAX_RATIO;
+    
+    speed.x += qx >> 8; if(speed.x>15) speed.x=15; if(speed.x<-15) speed.x=-15;
+    speed.y += qy >> 8; if(speed.y>15) speed.y=15; if(speed.y<-15) speed.y=-15;
+    speed.z += qz >> 8; if(speed.z>15) speed.z=15; if(speed.z<-15) speed.z=-15;
+  }
+  
+  //player.x += accel.x>>6;
+  //player.y -= accel.y>>6;
 
   layer_mark_dirty(graphics_layer);  // Schedule redraw of screen
 }
@@ -90,10 +114,46 @@ static void draw_stars(GContext *ctx, GRect frame) {
   graphics_context_set_stroke_color(ctx, 1); graphics_draw_rect(ctx, frame);                  //White Rectangle Border
 
   graphics_context_set_stroke_color(ctx, 1);
-  uint8_t tx, ty;
-  int16_t px, py;
+  //uint8_t tx, ty;
+  int32_t px, py, pz;
+  int32_t qx, qy, qz;
   
 	for(uint16_t i = 0; i < MAX_STARS; i++) {
+    px = star[i]->x;
+    py = star[i]->y;
+    pz = star[i]->z;
+    px -= 128; py -= 128; pz -= 128;
+    
+    qx = ((px * cos_lookup(a)) + (pz * sin_lookup(a))) / TRIG_MAX_RATIO;
+    qy = py;
+    qz = ((px * -sin_lookup(a)) + (pz * cos_lookup(a))) / TRIG_MAX_RATIO;
+    
+    px = qx;
+    py = ((qy * cos_lookup(b)) + (qz * -sin_lookup(b))) / TRIG_MAX_RATIO;
+    pz = ((qy * sin_lookup(b)) + (qz * cos_lookup(b))) / TRIG_MAX_RATIO;
+    
+    if(pz>0){
+      px = (px << 8) / pz;
+      py = (py << 8) / pz;
+      graphics_draw_pixel(ctx, GPoint(px + frame.origin.x + (frame.size.w/2), py + frame.origin.y + (frame.size.h / 2)));
+    }
+    
+    
+/*
+x' = (x * cos(a)) + (y * -sin(a))
+y' = (x * sin(a)) + (y * cos(a))
+z' = z
+Likewise, a rotation around the Y axis keeps the Y value alone, but involves trig functions on the X and Z values. Here's the function list for rotating around the Y axis:
+x' = (x * cos(a)) + (z * sin(a))
+y' = y
+z' = (x * -sin(a)) + (z * cos(a))
+... and rotation about the x axis uses a similar set of functions.
+x' = x
+y' = (y * cos(a)) + (z * -sin(a))
+z' = (y * sin(a)) + (z * cos(a))
+*/
+
+/*
     // take difference, loop over 65536
     tx = star[i]->x - player.x;
     ty = star[i]->y - player.y;
@@ -105,9 +165,9 @@ static void draw_stars(GContext *ctx, GRect frame) {
     // calculate pixel x and y
     px = (px << 8) / star[i]->z;
     py = (py << 8) / star[i]->z;
-    
+*/
     //graphics_draw_pixel(ctx, GPoint(px+72,py+84));
-    graphics_draw_pixel(ctx, GPoint(px + frame.origin.x + (frame.size.w/2), py + frame.origin.y + (frame.size.h / 2)));
+    
   }  
 
 }
@@ -117,7 +177,8 @@ static void graphics_layer_update(Layer *me, GContext *ctx) {
   draw_stars(ctx, GRect(1, 22, 142, 143));
   
   static char text[40];  //Buffer to hold text
-  snprintf(text, sizeof(text), "(%d,%d) %d", player.x, player.y, player.speed);  // What text to draw
+  //snprintf(text, sizeof(text), "(%d,%d) %d", player.x, player.y, player.speed);  // What text to draw
+  snprintf(text, sizeof(text), "%d %d", a, b);  // What text to draw
   draw_textbox(ctx, GRect(1, 1, 142, 20), text);
 
   app_timer_register(UPDATE_MS, timer_callback, NULL); // Schedule a callback
@@ -129,9 +190,9 @@ static void graphics_layer_update(Layer *me, GContext *ctx) {
 // ------------------------------------------------------------------------ //
 static void init_stars() {
 	for(uint16_t i = 0; i < MAX_STARS; i++) {
-    struct starstruct *tempstar = malloc(sizeof(struct starstruct));
-    star[i] = tempstar;
-    
+    struct xyz *temp = malloc(sizeof(struct xyz));
+    star[i] = temp;
+    // change to star[i] = malloc(sizeof(struct xyz));
 		star[i]->x = rand() & 255;
 		star[i]->y = rand() & 255;
     star[i]->z = rand() & 255;
@@ -175,7 +236,7 @@ static void init(void) {
   accel_data_service_subscribe(0, NULL);  // We will be using the accelerometer
   //font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_8));  // Load font
   init_stars();
-  player.x=0; player.y=0; player.speed=0; // init player
+  //player.x=0; player.y=0; player.speed=0; // init player
   //Begin
   window_stack_push(main_window, true /* Animated */); // Display window.  Callback will be called once layer is dirtied then written
 }
